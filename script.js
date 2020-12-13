60d5f0abde72235e2d6655fe5d5ca304585bdd64
Kirk <kirkbalop@gmail.com>
2020-12-13 12:02:29 -0800

    added concise and thorough arrays

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Assignment code here
 
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+// Create Arrays for character entries
+
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var characterArray = ["!","@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+// Empty Arrays 
+ 
+var resultArray = [];
+var userArray = [];
+
+uppercaseArray [1]
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
