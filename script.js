a5cfd2725b42ae37343c456d648def488d63aab5
Kirk <kirkbalop@gmail.com>
2020-12-13 14:17:40 -0800

    added remaining password friendly special characters

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function generatePassword() {
 var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var characterArray = ["!","@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+var characterArray = ["!","@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "'", "/", "|", "~", "{", "}", "[", "]"];
 
 // Empty Arrays 
  
